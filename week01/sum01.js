06130f7f070aa3dc2ea62eaf1bcab1d0c3fdba41
Amolnut055 <Amolnut055@users.noreply.github.com>
2025-08-18 16:37:19 +0700

    edit folder and edit file week01

--- week01/sum01.js
+++ week01/sum01.js
@@ -15,12 +15,13 @@ console.log (arr)
 for (i=0;i<arr.length;i++){
     console.log(arr[i])
 }
+
 // 7. shrink array with any operation  
 arr.pop()
 console.log(arr)
 // 8. apply spread on string variable
 let str="INT151" 
-const strCh=[...str] //เปลี่ยนstr เป้น arr
+const strCh=[...str] 
 console.log(strCh)
 // 9. create array with new constructor
 const arr1=new Array()
@@ -32,13 +33,13 @@ console.log(arr3)
 // create array with item 10
 const arr4=Array.of(10) 
 console.log(arr4)
-const arr5=Array.from(arr4) // copy arr4
+const arr5=Array.from(arr4) 
 console.log(arr5)
-const str2=Array.from(str) // เอาตัวแปรจากบรรทัดที่23
+const str2=Array.from(str) 
 console.log(str2)
 
 const [,,a,,...b]=[5,10,15,20,25]
 console.log(a) 
-console.log(typeof(a)) //number
+console.log(typeof(a)) 
 console.log(b) 
-console.log(typeof(b)) // object (array)
+console.log(typeof(b))
