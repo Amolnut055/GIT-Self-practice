15626aef7a43f2614733c4d8bdd1c55602ac5065
Amolnut055 <Amolnut055@users.noreply.github.com>
2025-09-06 17:49:19 +0700

    update

--- week02/source-code02.js
+++ week02/source-code02.js
@@ -112,3 +112,29 @@ console.log(isEqaulStd)
 console.log(studnet1 === studnet2);
 console.log(studnet1 === student3);
 
+// shallow equality
+  let book1 = {
+            isbn: 123456789 ,
+            title: "JavaScript"
+        }
+        let book2 = {
+            isbn: 123456789 ,
+            title: "JavaScript"
+        }
+
+        function shallowEquality(object1 , object2){
+            const key1 = object.key(object1)
+            const key2 = object.key(object2)
+
+            if(key1.length !== key2.length){
+                return false
+            }
+            for(let key of key1){
+                if(object1[key] !== object2[key]) {
+                    return false
+                }
+            }
+            return true
+        }
+
+        console.log("Shallow equality: " + shallowEquality(book1 , book2)) //True
